3ecb9c89323d31a97ed458daa3b07f3558710f6a
MuratO.Edutec <muratedutec@gmail.com>
2022-11-01 21:16:18 +0100

    4. commit

--- src/A04.java
+++ src/A04.java
@@ -1,2 +1,10 @@
 public class A04 {
+    /*
+    Bir projeyi ilk defa olusturdugumuzda
+    1-commit olustur
+    2-uzak masaustunde bir repo olustur
+    3- github'da istenen son 3 satiri copy yapip
+        porjeye oaste yapip calistirir
+        ----devami var EKSIK BU DOSYA
+     */
 }
